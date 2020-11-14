50c32c951045e3dea0502e826890d304d507c3cf
ThuZarAung <thuzar351996@gmail.com>
2020-11-14 19:08:08 +0630

    modified category

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -4,14 +4,13 @@
   <main class="app-content">
     <div class="app-title">
       <div>
-        <h1><i class="fa fa-dashboard"></i> Blank Page</h1>
-        <p>Start a beautiful journey here</p>
+          <h1> <i class="icofont-list"></i> Category </h1>
       </div>
-      <ul class="app-breadcrumb breadcrumb">
-        <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-        <li class="breadcrumb-item"><a href="#">Blank Page</a></li>
-      </ul>
-    </div>
+    <ul class="app-breadcrumb breadcrumb">
+      <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+      <li class="breadcrumb-item"><a href="{{route('category.index')}}">Category List</a></li>
+    </ul>
+  </div>
     <div class="row">
       <div class="col-md-12">
         <div class="tile">
